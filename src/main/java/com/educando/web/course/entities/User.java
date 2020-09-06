fb9cab65f20989031b5bbe76ed6645e2cec0090b
Juliano Hausen Oliveira da Costa <juliano.h.costa@rfb.gov.br>
2020-09-06 14:56:29 -0400

    commit implementa DataBase

--- src/main/java/com/educando/web/course/entities/User.java
+++ src/main/java/com/educando/web/course/entities/User.java
@@ -2,6 +2,11 @@ package com.educando.web.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,10 +16,13 @@ import lombok.ToString;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class User implements Serializable{
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
